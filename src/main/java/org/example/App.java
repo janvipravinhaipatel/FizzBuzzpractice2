d4b9d005b456a9dcb67021745ab29207e3b3a702
janvipravinhaipatel <158079903+janvipravinhaipatel@users.noreply.github.com>
2024-04-10 14:39:28 -0400

    second commit passed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,9 +7,13 @@ package org.example;
 public class App {
     public static String FizzBuzz(int a) {
         String Output = " ";
-        if(a%3==0 && a%5==0){
+        if(a%3==0 && a%5==0) {
             return ("FizzBuzz");
         }
+            else if(a%3==0){
+                return ("Fizz");
+            }
+
         return Output;
     }
     public static void main (String[]args){
